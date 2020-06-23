30b903147f2a0950f01e0423051597a349adb405
chu <chu@ittx.com.cn>
2020-06-23 14:41:19 +0800

    增加备案号

--- src/components/common/footer.vue
+++ src/components/common/footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="py-5 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Beauty Vayi 2019</p>
+      <p class="m-0 text-center text-white">Copyright &copy; <a class="text-center text-white" target="_blank" href="http://www.beian.miit.gov.cn"> 鄂ICP备18027192号-1</a></p>
     </div>
     <!-- /.container -->
   </footer>
